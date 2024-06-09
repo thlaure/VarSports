9c9f5cfbcf771c33308ba9550372d2011e756ea1
Thomas Laure <thomaslaure3@gmail.com>
2024-06-09 19:51:29 +0200

    feat display disciplines

--- src/Controller/AdminDisciplineController.php
+++ src/Controller/AdminDisciplineController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Discipline;
 use App\Form\DisciplineType;
+use App\Repository\DisciplineRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,17 +15,30 @@ use Symfony\Component\Routing\Attribute\Route;
 class AdminDisciplineController extends AbstractController
 {
     #[Route('/create', name: 'create')]
-    public function create(Request $request): Response
+    public function create(Request $request, DisciplineRepository $disciplineRepository, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(DisciplineType::class);
+        $discipline = new Discipline();
+        $form = $this->createForm(DisciplineType::class, $discipline);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $discipline = $form->getData();
+
+            try {
+                $entityManager->persist($discipline);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Succès');
+            } catch (\Exception $e) {
+                $this->addFlash('error', 'Une erreur est survenue');
+            }
+            
             return $this->redirectToRoute('app_admin_discipline_create');
         }
 
         return $this->render('admin/discipline/create.html.twig', [
             'form' => $form,
+            'disciplines' => $disciplineRepository->findAll(),
         ]);
     }
 }
