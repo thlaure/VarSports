d3dc78c44ae48f3956c151c3025a7fe3d08b56fb
Thomas Laure <thomaslaure3@gmail.com>
2024-07-10 21:58:22 +0200

    feat: edit admin data

---
+++ src/Controller/UserEditController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Constant\Message;
+use App\Form\UserEditType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class UserEditController extends AbstractController
+{
+    public function __construct(
+        private UserRepository $userRepository,
+        private EntityManagerInterface $entityManager,
+        private LoggerInterface $logger
+    ) {
+    }
+
+    #[Route('/user/{id}/edit', name: 'app_user_edit')]
+    #[IsGranted('ROLE_ADMIN_CLUB', message: Message::GENERIC_GRANT_ERROR)]
+    public function edit(int $id, Request $request): Response
+    {
+        $user = $this->userRepository->findOneBy(['id' => $id]);
+        if (null === $user) {
+            $this->logger->error(Message::DATA_NOT_FOUND);
+            throw $this->createNotFoundException(Message::DATA_NOT_FOUND);
+        }
+
+        $form = $this->createForm(UserEditType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $this->entityManager->flush();
+                $this->addFlash('success', Message::GENERIC_SUCCESS);
+
+                return $this->redirectToRoute('app_user_list');
+            } catch (\Exception $e) {
+                $this->logger->error($e->getMessage());
+                $this->addFlash('error', Message::GENERIC_ERROR);
+
+                return $this->redirectToRoute('app_user_edit');
+            }
+        }
+
+        return $this->render('admin/user/create_edit.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
---
+++ src/Form/UserEditType.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\PasswordStrength;
+
+class UserEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'required' => false,
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'required' => false,
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse e-mail',
+                'required' => false,
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Rôle',
+                'multiple' => false,
+                'expanded' => false,
+                'required' => false,
+                'choices' => [
+                    'Membre' => 'ROLE_MEMBER_CLUB',
+                    'Admin' => 'ROLE_ADMIN_CLUB',
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent être identiques',
+                'mapped' => false,
+                'required' => false,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => ['autocomplete' => 'new-password'],
+                    'constraints' => [
+                        new Length([
+                            'min' => 12,
+                            'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                        new PasswordStrength([
+                            'message' => 'Le mot de passe doit contenir au moins un chiffre, un caractère majuscule et un caractère minuscule.',
+                        ]),
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe',
+                    'attr' => ['autocomplete' => 'new-password'],
+                ],
+                'constraints' => [
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Soumettre',
+            ])
+        ;
+
+        $builder->get('roles')->addModelTransformer(new CallbackTransformer(
+            function ($rolesArray) {
+                // transform the array to a string
+                return count($rolesArray)? $rolesArray[0]: null;
+            },
+            function ($rolesString) {
+                // transform the string back to an array
+                return [$rolesString];
+            }
+        ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
