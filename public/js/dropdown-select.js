b60ca926f5c5f5e033d94b3f1ddfdbcc5b5e9430
Thomas Laure <thomaslaure3@gmail.com>
2024-07-24 09:24:16 +0200

    refactor: optimize dropdown select style and js

--- public/js/dropdown-select.js
+++ public/js/dropdown-select.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const labels = document.querySelectorAll('#search-clubs li label');
+    const labels = document.querySelectorAll('.dropdown-select .dropdown-select-label');
     for (const label of labels) {
         label.addEventListener('click', function(e) {
             e.stopPropagation();
