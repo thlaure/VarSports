4fa2ca886a9a514ebe4192667ff6a9149637e778
Thomas Laure <thomaslaure3@gmail.com>
2024-07-30 11:35:15 +0200

    fix: more permissive regex

--- src/Constant/Constraint.php
+++ src/Constant/Constraint.php
@@ -12,7 +12,7 @@ class Constraint
     public const REGEX_PHONE = '/(?:([+]\\d{1,4})[-.\\s]?)?(?:[(](\\d{1,3})[)][-.\\s]?)?(\\d{1,4})[-.\\s]?(\\d{1,4})[-.\\s]?(\\d{1,9})/';
     public const REGEX_POSTAL_CODE = '/^[0-9]{5}$/';
     public const REGEX_NAME = "/^[\\s\\p{Ll}\\p{Lu}\\p{M}\\-']+$/iu";
-    public const REGEX_COMMON = "/^[\\p{Ll}\\p{Lu}\\p{M}\\p{P}\\p{Sc}\\p{N}\\s\r\n\\(\\)\\/°\\+=]+$/iu";
+    public const REGEX_COMMON = "/^[\\p{Ll}\\p{Lu}\\p{M}\\p{P}\\p{Sc}\\p{N}\\s\r\n\\(\\)\\/°\\+=\\-'’]+$/iu";
 
     public const IMAGE_MAX_FILE_SIZE = 5 * 1024 * 1024; // 5 MB
     public const IMAGE_ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif', 'webp', 'avif'];
