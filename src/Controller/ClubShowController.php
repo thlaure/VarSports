f99c4ff45aa3d3e572b9f8b745d85899809d34da
Thomas Laure <thomaslaure3@gmail.com>
2024-08-21 11:49:41 +0200

    feat: display articles of club

--- src/Controller/ClubShowController.php
+++ src/Controller/ClubShowController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Club;
+use App\Repository\ArticleRepository;
 use App\Repository\ClubRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,7 +13,8 @@ use Symfony\Component\Routing\Attribute\Route;
 class ClubShowController extends AbstractController
 {
     public function __construct(
-        private ClubRepository $clubRepository
+        private ClubRepository $clubRepository,
+        private ArticleRepository $articleRepository
     ) {
     }
 
@@ -26,6 +28,7 @@ class ClubShowController extends AbstractController
 
         return $this->render('club/show.html.twig', [
             'club' => $club,
+            'articles' => $this->articleRepository->findBy(['club' => $club], ['creationDate' => 'DESC']),
         ]);
     }
 }
