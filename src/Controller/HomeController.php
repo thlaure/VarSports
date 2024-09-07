ee6f490ce9304957fdeb874cfd72dbce2961a340
Thomas Laure <thomaslaure3@gmail.com>
2024-09-07 15:42:10 +0200

    feat(Home): controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use App\Repository\ClubRepository;
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +12,20 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('', name: 'app_home_')]
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private ArticleRepository $articleRepository,
+        private EventRepository $eventRepository,
+        private ClubRepository $clubRepository
+    ) {
+    }
+
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->redirectToRoute('app_club_list');
+        return $this->render('home/index.html.twig', [
+            'lastCreatedClubs' => $this->clubRepository->findBy([], ['creationDate' => 'DESC'], 10),
+            'articles' => $this->articleRepository->findBy([], ['creationDate' => 'DESC'], 10),
+            'events' => $this->eventRepository->findBy([], ['startDate' => 'DESC'], 10),
+        ]);
     }
 }
