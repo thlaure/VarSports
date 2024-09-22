88e101499525bf5d3248b107209edb743da5f4ef
Thomas Laure <thomaslaure3@gmail.com>
2024-09-22 22:00:20 +0200

    feat(Legals): pages legals

--- src/Controller/LegalsController.php
+++ src/Controller/LegalsController.php
@@ -8,9 +8,27 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class LegalsController extends AbstractController
 {
-    #[Route('/mentions-legales', name: 'app_legals')]
-    public function index(): Response
+    #[Route('/mentions-legales', name: 'app_legals_mentions')]
+    public function legalMentions(): Response
     {
-        return $this->render('legals/index.html.twig');
+        return $this->render('legals/legals_mentions.html.twig');
+    }
+
+    #[Route('/cgu', name: 'app_legals_conditions_use')]
+    public function conditionsUse(): Response
+    {
+        return $this->render('legals/conditions_use.html.twig');
+    }
+
+    #[Route('/cgv', name: 'app_legals_conditions_sales')]
+    public function conditionsSales(): Response
+    {
+        return $this->render('legals/conditions_sales.html.twig');
+    }
+
+    #[Route('/confidentialite', name: 'app_legals_privacy_policy')]
+    public function privacyPolicy(): Response
+    {
+        return $this->render('legals/privacy_policy.html.twig');
     }
 }
