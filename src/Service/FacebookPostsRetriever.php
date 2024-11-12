e4b9c92bb0be58cf0b3bd461a45df1336c83a0c9
Thomas Laure <thomaslaure3@gmail.com>
2024-11-12 15:20:02 +0100

    feat(Facebook): retrieve posts

---
+++ src/Service/FacebookPostsRetriever.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Service;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class FacebookPostsRetriever
+{
+    public function __construct(
+        private LoggerInterface $logger,
+        private HttpClientInterface $httpClient,
+        private string $pageId,
+        private string $accessToken,
+    ) {
+    }
+
+    /**
+     * @return array<array<string, mixed>>
+     */
+    public function retrieve(): array
+    {
+        try {
+            $response = $this->httpClient->request(Request::METHOD_GET, "https://graph.facebook.com/v21.0/$this->pageId/feed", [
+                'query' => [
+                    'access_token' => $this->accessToken,
+                ],
+            ]);
+
+            return $response->toArray();
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+            throw $e;
+        }
+    }
+}
