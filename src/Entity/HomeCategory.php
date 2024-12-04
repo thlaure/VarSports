9bb2072a9452b3b4118a509cf3acd948a01289fd
Thomas Laure <thomas.laure-ext@lexisnexis.fr>
2024-12-04 19:17:52 +0100

    feat: add the profile homepage block

--- src/Entity/HomeCategory.php
+++ src/Entity/HomeCategory.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use App\Constant\Constraint;
-use App\Entity\Article;
 use App\Repository\HomeCategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
--- tests/Web/HomeControllerTest.php
+++ tests/Web/HomeControllerTest.php
@@ -12,6 +12,6 @@ class HomeControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'VarSports met en avant votre club');
+        $this->assertSelectorTextContains('h2', 'À propos de VarSports');
     }
 }
