44c0a1354741ca3c4cdaf53aa26bbe0209d3253e
Thomas Laure <thomas.laure-ext@lexisnexis.fr>
2024-12-23 08:55:27 +0100

    feat: articles social display

---
+++ public/js/home.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+      
+        slidesPerView: 3,
+        spaceBetween: 30,
+      
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 30
+            }
+        },
+      
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+      
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+      
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            hide: true,
+        },
+    });
+});
