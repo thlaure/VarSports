9ffe67b0509a75379675e716594041c645e36544
Thomas Laure <25416156+thlaure@users.noreply.github.com>
2025-01-18 16:24:12 +0100

    Refactor/homepage feedback (#92)
    * refactor(Homepage): enhance to note articles
    
    * refactor(Homepage): remove rounded on article to note
    
    * refactor(Home): rework article spotlight
    
    * refactor(Home): update articles display

--- public/js/home.js
+++ public/js/home.js
@@ -1,39 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const swiperArticlesSocial = new Swiper('.articles-social .swiper', {
+    const swiperArticlesToNote = new Swiper('.articles-to-note .swiper', {
         direction: 'horizontal',
         loop: true,
-      
-        slidesPerView: 3,
+
+        slidesPerView: 1,
         spaceBetween: 30,
-      
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 10
-            },
-            768: {
-                slidesPerView: 3,
-                spaceBetween: 20
-            },
-            1024: {
-                slidesPerView: 4,
-                spaceBetween: 30
-            }
-        },
-      
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-        },
-      
+
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-      
-        scrollbar: {
-            el: '.swiper-scrollbar',
-            hide: true,
+
+        a11y: {
+            prevSlideMessage: 'Previous slide',
+            nextSlideMessage: 'Next slide',
+        },
+    });
+
+    const swiperArticlesSocial = new Swiper('.articles-social .swiper', {
+        direction: 'horizontal',
+        loop: true,
+
+        slidesPerView: 1,
+        spaceBetween: 20,
+
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            1024: {
+                slidesPerView: 4,
+            }
+        },
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        a11y: {
+            prevSlideMessage: 'Previous slide',
+            nextSlideMessage: 'Next slide',
         },
     });
 });
