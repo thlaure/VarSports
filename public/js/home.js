0eae61529265be7a7242f20da3d50b7ba024c765
Thomas Laure <thomaslaure3@gmail.com>
2025-01-18 16:21:51 +0100

    refactor(Home): update articles display

--- public/js/home.js
+++ public/js/home.js
@@ -2,23 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const swiperArticlesToNote = new Swiper('.articles-to-note .swiper', {
         direction: 'horizontal',
         loop: true,
-      
+
         slidesPerView: 1,
         spaceBetween: 30,
-      
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-        },
-      
+
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
-        },
-      
-        scrollbar: {
-            el: '.swiper-scrollbar',
-            hide: true,
         },
 
         a11y: {
@@ -30,38 +20,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const swiperArticlesSocial = new Swiper('.articles-social .swiper', {
         direction: 'horizontal',
         loop: true,
-      
-        slidesPerView: 3,
-        spaceBetween: 30,
-      
+
+        slidesPerView: 1,
+        spaceBetween: 20,
+
         breakpoints: {
             320: {
                 slidesPerView: 1,
-                spaceBetween: 10
             },
             768: {
                 slidesPerView: 3,
-                spaceBetween: 20
             },
             1024: {
                 slidesPerView: 4,
-                spaceBetween: 30
             }
         },
-      
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-        },
-      
+
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
-        },
-      
-        scrollbar: {
-            el: '.swiper-scrollbar',
-            hide: true,
         },
 
         a11y: {
